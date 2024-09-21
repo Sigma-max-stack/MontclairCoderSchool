2ff26adabba93042f8ee3d25df3df6ba802cce8b
Sigma-max-stack <minibee0214@gmail.com>
2024-09-21 15:52:21 +0000

    Finish func prac day1

--- test.py
+++ test.py
@@ -3,7 +3,31 @@
     
 # print(add2(5, 6))
 
-def subtract2(x, y):
-    return x-y
+# def subtract2(x, y):
+#     return x-y
 
-print(subtract2(7 , 3))  
+# print(subtract2(7 , 3))  
+
+# def multi4(x):
+#     return 7*x
+
+# print(multi4(7))   
+
+# def squared(x):
+#     return x*x
+
+# print(squared(9))
+
+# def rectangleP(l, w):
+#     return l*2 + w*2
+# print(rectangleP(7, 3))
+
+# def rectangleA(l, w):
+#     return l*w
+
+# print(rectangleA(7, 3))
+
+def square(l):
+    return l*4+l*l
+
+print(square(5))
