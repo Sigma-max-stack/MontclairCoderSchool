688dafc593cfeca6c6517d6a01c8c401b842e869
Sigma-max-stack <minibee0214@gmail.com>
2024-09-28 15:37:49 +0000

    Day 2 practice

--- test.py
+++ test.py
@@ -1,33 +1,57 @@
-# def add2(x, y):
-#     return x+y
+# # def add2(x, y):
+# #     return x+y
     
-# print(add2(5, 6))
+# # print(add2(5, 6))
 
-# def subtract2(x, y):
-#     return x-y
+# # def subtract2(x, y):
+# #     return x-y
 
-# print(subtract2(7 , 3))  
+# # print(subtract2(7 , 3))  
 
-# def multi4(x):
-#     return 7*x
+# # def multi4(x):
+# #     return 7*x
 
-# print(multi4(7))   
+# # print(multi4(7))   
 
-# def squared(x):
-#     return x*x
+# # def squared(x):
+# #     return x*x
 
-# print(squared(9))
+# # print(squared(9))
 
-# def rectangleP(l, w):
-#     return l*2 + w*2
-# print(rectangleP(7, 3))
+# # def rectangleP(l, w):
+# #     return l*2 + w*2
+# # print(rectangleP(7, 3))
 
-# def rectangleA(l, w):
-#     return l*w
+# # def rectangleA(l, w):
+# #     return l*w
 
-# print(rectangleA(7, 3))
+# # print(rectangleA(7, 3))
 
-def square(l):
-    return l*4+l*l
+# def square(l):
+#     return l*4+l*l
 
-print(square(5))
+# print(square(5))
+
+# def three_add(x, y, z):
+#     return(x+y+z)
+
+# print(three_add(4,5,6))
+'''
+def op(x,y):
+    a = x*y
+    b = x+y
+    return(a+b)
+
+print(op(4, 5))    
+'''
+'''
+def op2(x, y):
+    a = x*y
+    b = x+y
+    if a>b:
+        return(a-b)
+    else:
+        return(b-a)
+print(op2(1, 1))
+'''
+
